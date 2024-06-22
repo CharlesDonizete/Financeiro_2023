5c2f4d61ed451e25190245bb5284965af082b101
Charles Donizete Alves dos Santos <82051427+CharlesDonizete@users.noreply.github.com>
2024-06-22 11:15:12 -0300

    Correção do Swagger para funcionar na publicação.

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -11,11 +11,9 @@ using Infra.Repositorio;
 using Infra.Repositorio.Generics;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Internal;
 using Microsoft.IdentityModel.Tokens;
-using WebApi;
+using Microsoft.OpenApi.Models;
 using WebApi.Token;
-using static System.Net.Mime.MediaTypeNames;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,7 +22,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => { 
+     c.SwaggerDoc("v1", new OpenApiInfo { Title = "Financeiro_2023", Version = "v1" });
+});
 
 builder.Services.AddDbContext<ContextBase>(options =>
     options.UseSqlServer(
@@ -82,10 +82,10 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() )
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwagger();    
+    app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "Financeiro_2023"); });
 }
 
 var devClient = "http://localhost:4200";
