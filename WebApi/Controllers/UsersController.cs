bcbe071d6bfd13f42f39576fbcff8f52edbcd632
Charles Donizete Alves dos Santos <82051427+CharlesDonizete@users.noreply.github.com>
2024-07-27 11:47:26 -0300

    Criado os endpoints de manutenção de usuário.

--- WebApi/Controllers/UsersController.cs
+++ WebApi/Controllers/UsersController.cs
@@ -1,6 +1,5 @@
 ﻿using Entities.Entidades;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.WebUtilities;
@@ -59,6 +58,69 @@ namespace WebApi.Controllers
                 return Ok("Usuário Adicionado");
 
             return Ok("Erro ao confirmar cadastro de usuário!");
+        }
+
+        [HttpPut("/api/AtualizaUsuario/{id}")]
+        public async Task<IActionResult> AtualizaUsuario(string id, [FromBody] LoginModel login)
+        {
+            if (string.IsNullOrWhiteSpace(id) || string.IsNullOrWhiteSpace(login.email) || string.IsNullOrWhiteSpace(login.cpf))
+                return BadRequest("Faltam alguns dados.");
+            
+            var user = await _userManager.FindByIdAsync(id);
+
+            if (user == null)
+                return NotFound("Usuário não encontrado.");
+            
+            user.CPF = login.cpf;
+
+            //// Atualiza a senha se fornecida
+            //if (!string.IsNullOrWhiteSpace(login.senha))
+            //{
+            //    var newPasswordHash = _userManager.PasswordHasher.HashPassword(user, login.senha);
+            //    user.PasswordHash = newPasswordHash;
+            //}
+
+            var result = await _userManager.UpdateAsync(user);
+
+            if (result.Succeeded)
+                return Ok("Usuário atualizado com sucesso!");
+            else
+                return BadRequest(result.Errors);            
+        }
+
+
+        [HttpDelete("/api/DeletaUsuario/{id}")]
+        public async Task<IActionResult> DeletaUsuario(string id)
+        {
+            if (string.IsNullOrWhiteSpace(id))
+                return BadRequest("ID do usuário não fornecido.");            
+
+            var user = await _userManager.FindByIdAsync(id);
+
+            if (user == null)
+                return NotFound("Usuário não encontrado.");
+            
+            var result = await _userManager.DeleteAsync(user);
+
+            if (result.Succeeded)
+                return Ok("Usuário deletado com sucesso!");
+            else
+                return BadRequest(result.Errors);            
+        }
+
+        [HttpGet("/api/ListaUsuarios")]
+        public IActionResult ListaUsuarios()
+        {
+            var users = _userManager.Users.ToList();
+            
+            var simplifiedUserList = users.Select(user => new
+            {
+                UserId = user.Id,
+                Email = user.Email,
+                CPF = user.CPF                
+            });
+
+            return Ok(simplifiedUserList);
         }
     }
 }
